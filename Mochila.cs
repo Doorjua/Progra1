dc5b859b74738b7676603cd6c28582bb29c9cd84
Doorjua <142053941+Doorjua@users.noreply.github.com>
2023-08-18 12:59:26 -0600

    PrimerPractica

---
+++ Mochila.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace ConsoleApplication1
+{
+    class Mochila
+    {
+
+        int NumDeBolsas;
+        string Tamanio;
+        float Precio;
+        string Disenio;
+        string Material;
+        int NumDeLlaveros;
+        Color color;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        //Este es el metodo main.
+        //Sirve com punto de entrada para el programa
+        //(inicia desde aqui)
+        static void Main(string[] args)
+        {
+            //Esto es una instancia de Mochila
+            Mochila mochilaAna = new Mochila();
+            mochilaAna.Disenio = "Piolin";
+            mochilaAna.Material = "Vinyl";
+            mochilaAna.NumDeBolsas = 3;
+            mochilaAna.NumDeLlaveros = 0;
+            mochilaAna.Precio = 500.000f;
+            mochilaAna.color.Rojo = 100;
+            mochilaAna.color.Verde = 20;
+            mochilaAna.colo.Azul = 33;
+        }
+    }
+}
