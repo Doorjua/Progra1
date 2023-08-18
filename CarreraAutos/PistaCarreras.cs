d5b9854d8099bd9b5e4a6218c9d196dcd3bfc1c1
Doorjua <142053941+Doorjua@users.noreply.github.com>
2023-08-18 14:54:50 -0600

    Carrera
    por fin

---
+++ CarreraAutos/PistaCarreras.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarreraAutos
+{
+    class PistaCarreras
+    {
+        public string nombre;
+        public float longitud;
+
+        public void Competir(AutoCarreras auto1, AutoCarreras auto3)
+        {
+            Console.WriteLine("Iniciando carrera entre" 
+                + auto1.modelo + "y" + auto3.modelo);
+            
+            if(auto1.velocidad > auto3.velocidad)
+            {
+                Console.WriteLine("Gano "+ auto1.modelo);
+            }
+            else if (auto3.velocidad > auto1.velocidad)
+            {
+                 Console.WriteLine("Gano "+ auto3.modelo);
+            }
+            else
+                 //tienen la misma velocidad
+            {
+                 Console.WriteLine("Hubo empate");
+            }
+        }
+
+    }
+}
---
+++ CarreraAutos/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarreraAutos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AutoCarreras ferrari = new AutoCarreras();
+            ferrari.modelo = "Ferrati F150";
+            ferrari.peso = 1.3f;
+            ferrari.velocidad = 230;
+
+            AutoCarreras lamborgini = new AutoCarreras();
+            lamborgini.modelo = "Lamborgini Aventador";
+            lamborgini.peso = 1.4f;
+            lamborgini.velocidad = 255;
+
+            AutoCarreras mcLaren = new AutoCarreras();
+            mcLaren.modelo = "McLaren 540c";
+            mcLaren.peso = 1.6f;
+            mcLaren.velocidad = 240;
+
+            PistaCarreras hnsRodriguez = new PistaCarreras();
+            hnsRodriguez.nombre = "Autodromo Hermanos Rodriguez";
+            hnsRodriguez.longitud = 5.6f; //km
+
+            //Mando a competir a los autos
+            hnsRodriguez.Competir(ferrari, lamborgini);
+
+            hnsRodriguez.Competir(ferrari, mcLaren);
+
+            hnsRodriguez.Competir(mcLaren, lamborgini);
+
+            //Esta instruccion espera a que des enter
+            //para continuar
+            Console.ReadLine();
+        }
+    }
+}
